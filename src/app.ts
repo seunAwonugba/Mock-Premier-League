191d96062c278b25ede09760c155bbee19fa7cf8
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2024-09-27 04:19:48 +0100

    db and redis set up

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import "dotenv/config";
 import express from "express";
+import sequelize from "./models";
+import { initializeRedisClient } from "./redis";
 
 const app = express();
 
@@ -9,6 +11,22 @@ const host = String(HOST);
 
 app.use(express.json());
 
-app.listen(port, host, () => {
-    console.log(`⚡️[server]: Server is running at http://${host}:${port}`);
-});
+const startServer = async () => {
+    try {
+        await sequelize!.authenticate();
+        console.log(
+            "⚡️[database]: Database connection has been established successfully."
+        );
+        await initializeRedisClient();
+
+        app.listen(port, host, () => {
+            console.log(
+                `⚡️[server]: Server is running at http://${host}:${port}`
+            );
+        });
+    } catch (error) {
+        console.log("😥 [database]: Unable to connect to the database:", error);
+    }
+};
+
+startServer();
---
+++ src/redis/index.ts
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { RedisClientType, createClient } from "redis";
+
+let client: RedisClientType;
+
+export const initializeRedisClient = async () => {
+    client = createClient({
+        url: process.env.REDIS_URL,
+        socket: {
+            port: Number(process.env.REDIS_PORT),
+        },
+    });
+
+    try {
+        await client.connect();
+        console.log(
+            "⚡️[redis]: Redis connection has been established successfully."
+        );
+    } catch (error) {}
+};
+
+export const isRedisWorking = () => {
+    // verify wheter there is an active connection
+    // to a Redis server or not
+    return !!client?.isOpen;
+};
+
+export const getRedisCache = async (key: string) => {
+    if (isRedisWorking()) {
+        try {
+            return await client.get(key);
+        } catch (error) {}
+    }
+};
+
+export const setRedisCache = async (key: string, value: string) => {
+    if (isRedisWorking()) {
+        try {
+            return await client.set(key, value);
+        } catch (error) {}
+    }
+};
+
+export const deleteCache = async (key: string) => {
+    if (isRedisWorking()) {
+        try {
+            return await client.del(key);
+        } catch (error) {}
+    }
+};
+
+export const setWithExpiry = async (key: any, value: any, expiry: any) => {
+    if (isRedisWorking()) {
+        try {
+            return await client.set(key, value, "EX", expiry);
+        } catch (error) {}
+    }
+};
