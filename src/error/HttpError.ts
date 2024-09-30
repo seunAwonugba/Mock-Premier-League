4cc012a24edb2031a9f7cebcf784a4b7dbb8d9a8
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2024-09-30 14:05:30 +0100

    add docker to project

--- src/error/HttpError.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosError } from "axios";
-
-function isAxiosError<T>(error: Error | AxiosError<T>): error is AxiosError<T> {
-    return "isAxiosError" in error && error.isAxiosError;
-}
-
-export { isAxiosError };
